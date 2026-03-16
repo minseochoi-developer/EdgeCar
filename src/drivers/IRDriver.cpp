10e709255d9478a0892de27013a0e9929b69200b
chlalstj1999 <alstjsms23@gmail.com>
2026-03-16 19:02:00 +0900

    fix : switch IRremote to Timer1 to resolve PWM conflict on D3/D11

--- src/drivers/IRDriver.cpp
+++ src/drivers/IRDriver.cpp
@@ -1,7 +1,8 @@
+#define IR_USE_AVR_TIMER1
+#include <Arduino.h>
+#include <IRremote.hpp>
 #include "IRDriver.h"
 #include "../common/pins.h"
-#include <IRremote.hpp>
-#include <Arduino.h>
 
 void IRDriver::begin() {
     IrReceiver.begin(PIN_IR_RECEIVER);
